c9d24a04a285cbbcbd5b0135299292708139159a
aslfasl <volkov0989@outlook.com>
2022-12-22 13:24:15 +0300

    Initial Commit

---
+++ WebParent/Backend/src/main/java/ru/shopproject/backend/admin/MainController.java
@@ -0,0 +1,13 @@
+package ru.shopproject.backend.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String getHomePage() {
+        return "index";
+    }
+}
---
+++ WebParent/Frontend/src/main/java/ru/shopproject/frontend/FrontendApplication.java
@@ -0,0 +1,13 @@
+package ru.shopproject.frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrontendApplication.class, args);
+    }
+
+}
